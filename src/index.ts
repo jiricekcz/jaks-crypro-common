ddcbf5aa478b92aabd7061336c5de9df14792663
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2022-09-23 20:57:10 +0200

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
-import { CryptoClient, Key} from "./cryptoClient";
+import { CryptoClient, Key, AsymmetricDSAlgorithm, KEY_TYPES, KeyAlgorithmsMap, KeyPair, KeyType, PrivateKey, PublicKey, Signature, SignedData, SymmetricDSAlgorithm, SymmetricKey } from "./cryptoClient";
 import * as JWKStandard from "./jwk";
-
+import { DataResponse, StandardDataResponse } from "./dataResponse";
 export {
     CryptoClient,
     Key,
-    JWKStandard
+    JWKStandard,
+    AsymmetricDSAlgorithm,
+    KEY_TYPES,
+    KeyAlgorithmsMap,
+    KeyPair,
+    KeyType,
+    PrivateKey,
+    PublicKey,
+    Signature,
+    SignedData,
+    SymmetricDSAlgorithm,
+    SymmetricKey,
+    DataResponse,
+    StandardDataResponse
 }
